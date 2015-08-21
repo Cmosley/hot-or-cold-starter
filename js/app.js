0b3b06b95b24578243c259baffeea660fa3836d5
Christian Mosley <cmosley@beholdmedia.com>
2015-08-20 20:48:51 -0500

    newgame

--- js/app.js
+++ js/app.js
@@ -12,6 +12,36 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+
+	$(".new").click(function () {
+		$(function(){
+			newGame(); 
+		});
+		console.log("starting new game");
+	});
+	
+
+	/* Generate random number 1 - 100 */
+
+	var secretNum = function getSecretNum(min, max) {
+			return Math.floor((Math.random() * 100) + 1); 
+			console.log(secretNum);
+	}
+
+	
+	function newGame() {
+
+		$('#userGuess').attr('placeholder', 'Enter your Guess'); 
+		
+		$('#count').text("0"); 	
+
+		$('#guesslist').empty(); 
+
+	};
+
+	 newGame();
+
 });
 
 
