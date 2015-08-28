949e662853bff52d3cc0fc7557aabd0626fe1765
Christian Mosley <cmosley@beholdmedia.com>
2015-08-28 09:42:01 -0500

    clean up

--- js/app.js
+++ js/app.js
@@ -24,14 +24,13 @@ $(document).ready(function(){
 
 	/* Generate random number 1 - 100 */
 
-	
-
-	
-	
+		
 
 });
 
- /* New game and its required functions */
+
+
+ /* New game and its required functions - Code from first sample website */
 
 	function newGame() {
 		resetGuessList();
@@ -121,7 +120,7 @@ $(document).ready(function(){
 	}
 
 	
-/*
+/*  Hot + cold example http://scotts0rensen.github.io/hot-or-cold/#
 
 			function newGame() {
 			  resetGuessCount();
@@ -198,3 +197,110 @@ $(document).ready(function(){
 			}
 
 			*/
+
+
+
+
+
+
+
+
+
+/*  Hot + cold example http://andraroston.github.io/hot-or-cold-app/index.html#
+
+$(document).ready(function(){
+
+
+	var answerNumber = function() {
+			computerNumber = Math.floor(Math.random() * 101);
+				console.log(computerNumber);
+				return computerNumber;
+	};
+
+	answerNumber();
+
+	var counter = 0;
+	var counting = function(){
+  		counter += 1;
+   		$('#count').replaceWith("<span id='count'>" + counter + "</span>");
+  	};
+
+	function gamePlay() { 
+
+		var guessNumber = $("#userGuess").val();
+			console.log(guessNumber);	
+
+		computerNumber;
+
+		var guessChecker = (Math.abs(guessNumber - computerNumber));
+			console.log(guessChecker);
+
+
+		if(guessChecker === 0) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You got it right - press the New Game button to start again!" + "</h2>");
+		}
+
+		else if(guessChecker >= 30 && guessChecker <=49) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're cold!" + "</h2>");
+		}
+
+		else if(guessChecker >= 20 && guessChecker <=29) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're warm!" + "</h2>");
+		}
+
+		else if(guessChecker >= 10 && guessChecker <=19) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're hot!" + "</h2>");
+		}
+
+		else if(guessChecker >= 1 && guessChecker <=9) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're burning up!" + "</h2>");
+		}
+
+		else {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're freezing!" + "</h2>");
+		}
+
+	};
+
+
+	$("#guessButton").on("click", function(){
+		
+		event.preventDefault();
+
+		var guessNumber = $("#userGuess").val();
+			if(guessNumber > 0 && guessNumber < 101) {
+				$("#guessList").append("<li>" + guessNumber + "</li>");
+				gamePlay();
+				counting();
+			} else {
+			alert("Not a valid number, try again!")
+			}
+			$("#userGuess").val(" ");
+		
+	}); 
+
+	$(".new").on("click", function() {
+		$("#guessList").empty();
+		answerNumber();
+		var counter = 0;
+		$("#feedback").replaceWith("<h2 id='feedback'>" + "Make your Guess!" + "</h2>");
+		$('#count').replaceWith("<span id='count'>" + 0 + "</span>");
+
+	})
+
+	--- Display information modal box ---
+
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	--- Hide information modal box ---
+
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+
+});
+
+*/
