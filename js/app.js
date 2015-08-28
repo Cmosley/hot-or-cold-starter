3a633e8790c819824da3faf130c6ae799ee8cfa7
Christian Mosley <cmosley@beholdmedia.com>
2015-08-28 09:37:39 -0500

    added two sample codes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 
+var guessCount = 0; 
+var guess;
+
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -13,35 +16,185 @@ $(document).ready(function(){
   	});
 
 
-
-	$(".new").click(function () {
-		$(function(){
-			newGame(); 
-		});
+  	/* Start new game */ 
+	$(".new").click( newGame );
 		console.log("starting new game");
-	});
 	
+	newGame;
 
 	/* Generate random number 1 - 100 */
 
-	var secretNum = function getSecretNum(min, max) {
-			return Math.floor((Math.random() * 100) + 1); 
-			console.log(secretNum);
-	}
+	
 
 	
-	function newGame() {
-
-		$('#userGuess').attr('placeholder', 'Enter your Guess'); 
-		
-		$('#count').text("0"); 	
-
-		$('#guesslist').empty(); 
-
-	};
-
-	 newGame();
+	
 
 });
 
+ /* New game and its required functions */
 
+	function newGame() {
+		resetGuessList();
+		resetGuessCount();
+		getSecretNum();
+		setFeedback('Make your Guess!');
+	}
+
+		
+		function resetGuessList() {
+			$('#guesslist').empty(); 
+		}
+
+		function resetGuessCount() {
+ 	 	setGuessCount(0);
+		}
+
+		function getSecretNum(min, max) {
+				return Math.floor((Math.random() * 100) + 1); 
+				console.log(secretNum);
+		}
+
+			function setFeedback(feedback) {
+			  $("h2#feedback").text(feedback);
+			}
+
+			function getFeedback(guess) {
+	  		var feedback;
+
+
+/* Functions required to handle user guesses */
+
+
+	function guessHandler(event) {
+	  event.preventDefault();
+
+	  incrementGuessCount();
+	  var guess = getGuess();
+	  addGuessToList(guess);
+	  setFeedback( getFeedback(guess) );
+
+	  clearGuess();
+
+			function incrementGuessCount() {
+			  setGuessCount(guessCount+1);
+			}
+
+			function getGuess() {
+			  return $("#userGuess").val();
+			}
+
+			function addGuessToList(guess) {
+			  var listItem = $("<li>" + guess + "</li>");
+			  $("#guessList").append(listItem);
+			}
+
+			function clearGuess() {
+		 	 $("#userGuess").val("");
+			}
+
+	
+
+	
+
+	function setGuessCount(guessCnt) {
+	  guessCount = guessCnt;
+	  $("span#count").text(guessCount);
+	}
+
+
+	function getFeedback(guess) {
+	  var feedback;
+
+	  var diff = Math.abs(guess-answer);
+	  if (diff >= 50) feedback = "Ice Cold";
+	  else if (diff >= 30) feedback = "Cold";
+	  else if (diff >= 20) feedback = "Warm";
+	  else if (diff >= 10) feedback = "Hot";
+	  else if (diff >= 5) feedback = "Very Hot";
+	  else if (diff >= 1) feedback = "Burning Up!";
+	  else feedback = "Correct! You win!";
+
+	  if (diff > 0)
+	    feedback += " (you are too " + (guess > answer ? "high" : "low") + ")";
+
+	  return feedback;
+	}
+
+	
+/*
+
+			function newGame() {
+			  resetGuessCount();
+			  generateAnswer();
+			  setFeedback("Make your guess!");
+			  resetGuessList();
+			}
+
+			function guessHandler(event) {
+			  event.preventDefault();
+
+			  incrementGuessCount();
+			  var guess = getGuess();
+			  addGuessToList(guess);
+			  setFeedback( getFeedback(guess) );
+
+			  clearGuess();
+			}
+
+			function resetGuessCount() {
+			  setGuessCount(0);
+			}
+
+			function incrementGuessCount() {
+			  setGuessCount(guessCount+1);
+			}
+
+			function setGuessCount(guessCnt) {
+			  guessCount = guessCnt;
+			  $("span#count").text(guessCount);
+			}
+
+			function setFeedback(feedback) {
+			  $("h2#feedback").text(feedback);
+			}
+
+							function generateAnswer() {
+							  answer = Math.floor((Math.random() * MAXIMUM_ANSWER) + 1);
+							}
+
+			function clearGuess() {
+			  $("#userGuess").val("");
+			}
+
+			function getGuess() {
+			  return $("#userGuess").val();
+			}
+
+			function resetGuessList() {
+			  $("#guessList").empty();
+			}
+
+			function addGuessToList(guess) {
+			  var listItem = $("<li>" + guess + "</li>");
+			  $("#guessList").append(listItem);
+			}
+
+			function getFeedback(guess) {
+			  var feedback;
+
+			  var diff = Math.abs(guess-answer);
+			  if (diff >= 50) feedback = "Ice Cold";
+			  else if (diff >= 30) feedback = "Cold";
+			  else if (diff >= 20) feedback = "Warm";
+			  else if (diff >= 10) feedback = "Hot";
+			  else if (diff >= 5) feedback = "Very Hot";
+			  else if (diff >= 1) feedback = "Burning Up!";
+			  else feedback = "Correct! You win!";
+
+			  if (diff > 0)
+			    feedback += " (you are too " + (guess > answer ? "high" : "low") + ")";
+
+			  return feedback;
+			}
+
+			*/
